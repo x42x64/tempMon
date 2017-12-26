44cc8e54a852cfba812697da8eb63f3d5446cc68
x42x64 <martin-jan@arcor.de>
2017-12-26 14:27:23 +0000

    added script to move data from ramdisk to network storage. Script mounts the remote storage, copies all except the current log, removes them if successful and unmounts the remoute storage again.

---
+++ mv2loggingstore.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+shopt -s extglob
+LOGGER_DIR=/tmp/logging/logger
+
+cd $LOGGER_DIR
+
+LAST_FILE=$(ls -1 | tail -n1)
+echo $LAST_FILE
+
+mount quh:/volume1/Logging/heizung /mnt/quh 
+
+if [ $? -eq 0 ]; then
+	cp -r !($LAST_FILE) /mnt/quh
+	if [ $? -eq 0 ]; then
+		rm -r !($LAST_FILE)
+	else
+		echo "Could not copy data to network storage"
+	fi
+
+	umount /mnt/quh
+fi
