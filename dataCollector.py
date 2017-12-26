ed2291c9d1e9dfa0624e2fedaf5459d33d94bd75
Jan <martin-jan@arcor.de>
2017-12-26 07:45:32 +0100

    implemented thread to serve current temperature over http/json.

--- dataCollector.py
+++ dataCollector.py
@@ -4,6 +4,8 @@ import time
 import os
 import threading
 
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
 SENSOR_PATHS = dict()
 SENSOR_PATHS["DG_Lead"] = "28-00000529cf95"
 SENSOR_PATHS["DG_Return"] = "28-0000052997b1"
@@ -59,6 +61,48 @@ class DataLogging(DataCollectorCallback):
             json.dump(extData, file)
             file.write("\n")
 
+class httpProvider(threading.Thread):
+    dc = None
+    lock = threading.Lock()
+
+    def __init__(self, dataCollector):
+        threading.Thread.__init__(self)
+        self.endRequest = False
+        httpProvider.dc = dataCollector
+
+
+    class httpServer_RequestHandler(BaseHTTPRequestHandler):
+        # GET
+        def do_GET(self):
+            # Send response status code
+            self.send_response(200)
+
+            # Send headers
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            # Send message back to client
+            message = json.dumps(httpProvider.dc.getCurrentData())
+            # Write content as utf-8 data
+            self.wfile.write(bytes(message, "utf8"))
+            return
+
+    def stopRequest(self):
+        self.httpd.shutdown()
+
+    def run(self):
+        print('starting server...')
+
+        # Server settings
+        # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+        server_address = ('127.0.0.1', 8081)
+        self.httpd = HTTPServer(server_address, httpProvider.httpServer_RequestHandler)
+        print('running server...')
+
+        try:
+            self.httpd.serve_forever()
+        finally:
+            self.httpd.server_close()
 
 
 def clearScreen():
@@ -86,8 +130,8 @@ class DataCollector(threading.Thread):
         self.sensors["HeatResorvoir2a"] = sensor.ds1820(testSensorPath)
         self.sensors["HeatResorvoirReturnBoiler"] = sensor.ds1820(testSensorPath)
 
-        for k in SENSOR_PATHS.keys():
-            self.sensors[k] = sensor.ds1820(getW1Path(SENSOR_PATHS[k]))
+        #for k in SENSOR_PATHS.keys():
+        #    self.sensors[k] = sensor.ds1820(getW1Path(SENSOR_PATHS[k]))
 
         self.sensors["Ambient"]= sensor.ds1820(testSensorPath)
         self.sensors["Outside"] = sensor.ds1820(testSensorPath)
@@ -155,22 +199,25 @@ class DataCollector(threading.Thread):
 
 def main():
     dc = DataCollector()
+    httpd = httpProvider(dc)
     datalogger = DataLogging("/tmp/logger2.log")
     dc.addCallback(datalogger)
     dc.start()
+    httpd.start()
 
 
     while True:
         try:
-            clearScreen()
+            #clearScreen()
             data = dc.getCurrentData()
-            for k in data.keys():
-                print(k + ": \t" + str(data[k]["value"]) + "°C")
+            #for k in data.keys():
+            #    print(k + ": \t" + str(data[k]["value"]) + "°C")
 
             time.sleep(1)
 
         except KeyboardInterrupt:
             dc.stopRequest()
+            httpd.stopRequest()
             print("Stop might take up to 30s. Please wait...")
             break
         except:
@@ -178,6 +225,8 @@ def main():
 
 
     dc.join()
+    print("DC done")
+    httpd.join()
     print("Application ended.")
 
 #solarController = hysteresis(20.0, 15.0)
