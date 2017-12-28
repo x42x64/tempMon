5118f6d3e67b2abc5da3c30680bc05451bea05a2
x42x64 <martin-jan@arcor.de>
2017-12-28 11:21:34 +0000

    Adding a script to run the datalogger on RPi

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+LOGGER_IP=$(ip addr show wlan0 | grep "inet " | cut -d' ' -f 6 | cut -d'/' -f 1) LOGGER_DIR=/mnt/ramdsk/logger MPLBACKEND=Agg nohup python3 dataCollector.py > /dev/null &
