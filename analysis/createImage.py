ba0d3c2e7152b629c70f3794e607e10579132ec9
x42x64 <martin-jan@arcor.de>
2017-12-28 11:15:57 +0000

    adjusting paths for RPi

--- analysis/createImage.py
+++ analysis/createImage.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import time
 from loadLogIntoPandas import loadLogsAsDF
 
-df = loadLogsAsDF('/media/jan/quh/logging/heizung/', start=time.time()-24*60*60.0)
+df = loadLogsAsDF('/mnt/quh/', start=time.time()-24*60*60.0)
 df_plot = df[['data.Heat_Resorvoir0.value', 'data.Heat_Resorvoir1.value', 'data.Heat_Resorvoir2.value', 'data.Heat_Resorvoir3.value', 'data.SolarHeatExchangeLead.value', 'data.SolarHeatExchangeReturn.value']]
 ax = df_plot.plot()
 ax.grid(True)
@@ -11,9 +11,8 @@ plt.xlabel("UTC time (m-d h)")
 fig = plt.gcf()
 fig.set_size_inches(10, 5)
 ax.legend_.remove()
-plt.savefig("graphHeatResorvoir.png")
+plt.savefig("/mnt/ramdsk/graphHeatResorvoir.png")
 
-print(list(df_plot))
 fig_legend = plt.figure(figsize=(3.5, 1.5))
 fig_legend.legend(ax.get_lines(), list(df_plot), loc='center', frameon=False)
-plt.savefig("graphHeatResorvoirLegend.png")
+plt.savefig("/mnt/ramdsk/graphHeatResorvoirLegend.png")
