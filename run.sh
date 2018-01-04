f1d365f1292456eafd48be3f62215491089bfda9
x42x64 <martin-jan@arcor.de>
2018-01-04 16:03:25 +0000

    creating dir in ramdisk upfront

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+mkdir /mnt/ramdsk/logger
 LOGGER_IP=$(ip addr show wlan0 | grep "inet " | cut -d' ' -f 6 | cut -d'/' -f 1) LOGGER_DIR=/mnt/ramdsk/logger MPLBACKEND=Agg nohup python3 dataCollector.py > /dev/null &
